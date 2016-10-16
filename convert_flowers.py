d184d55175cbbf91e0bdc9d1a5f193ae81cc13c7
Ramon Wijnands <rayman747@hotmail.com>
2016-10-16 15:12:33 +0200

    Refactor convert_flowers

--- convert_flowers.py
+++ convert_flowers.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 
 import errno
 import glob
+import logging
 import os
 import re
 from argparse import ArgumentParser
@@ -22,11 +23,17 @@ int64_feature = lambda v: tf.train.Feature(int64_list=tf.train.Int64List(value=[
 
 
 def mkdir_p(path):
+    """mkdir -p
+
+    :param path:
+    :return: True if it had to create the directory
+    """
     try:
         os.makedirs(path)
+        return True
     except OSError as exc:  # Python >2.5
         if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
+            return False
         else:
             raise
 
@@ -91,48 +98,41 @@ def get_dataset_filename(dataset_dir, split_name, shard_id, num_shards):
     return os.path.join(dataset_dir, output_filename)
 
 
-def convert_dataset(split_name, filenames, class_names_to_ids, dataset_dir):
+def convert_dataset(split_name, filenames, class_ids, dataset_dir):
     assert split_name in ['train', 'validation']
     print('Converting %s dataset' % split_name)
 
     num_per_shard = 1000
 
-    with tf.Graph().as_default():
-        # Initializes function that decodes RGB JPEG data.
-        decode_jpeg_data = tf.placeholder(dtype=tf.string)
-        decode_jpeg = tf.image.decode_jpeg(decode_jpeg_data, channels=3)
+    # Initializes function that decodes RGB JPEG data.
+    decode_jpeg_data = tf.placeholder(dtype=tf.string)
+    decode_jpeg = tf.image.decode_jpeg(decode_jpeg_data, channels=3)
 
-        with tf.Session() as sess:
-            # process chunks of 1000
-            for shard_id, files in enumerate(tqdm(chunk(filenames, num_per_shard),
-                                                  desc='Shards', unit='shard')):
-                output_filename = get_dataset_filename(dataset_dir, split_name, shard_id,
-                                                       len(filenames) // num_per_shard)
+    with tf.Session() as sess:
+        # process chunks of 1000
+        for i, (filename, class_id) in enumerate(tqdm(zip(filenames, class_ids), desc='Images', unit='image')):
+            shard_id = i // num_per_shard
+            output_filename = get_dataset_filename(dataset_dir, split_name, shard_id, len(filenames) // num_per_shard)
 
-                for i, filename in enumerate(tqdm(files,
-                                                  leave=False, desc='Images', unit='image')):
-                    with tf.python_io.TFRecordWriter(output_filename) as tfrecord_writer:
-                        # Read the filename:
-                        image_data = open(filename, 'r').read()
+            with tf.python_io.TFRecordWriter(output_filename) as tfrecord_writer:
+                # Read the filename:
+                image_data = open(filename, 'r').read()
 
-                        # decode JPEG
-                        image = sess.run(decode_jpeg, feed_dict={decode_jpeg_data: image_data})
-                        assert len(image.shape) == 3
-                        assert image.shape[2] == 3
-                        height, width = image.shape[0], image.shape[1]
+                # decode JPEG
+                image = sess.run(decode_jpeg, feed_dict={decode_jpeg_data: image_data})
+                assert len(image.shape) == 3
+                assert image.shape[2] == 3
+                height, width = image.shape[0], image.shape[1]
 
-                        class_name = os.path.basename(os.path.dirname(filename))
-                        class_id = class_names_to_ids[class_name]
+                example = tf.train.Example(features=tf.train.Features(feature={
+                    'image/encoded': bytes_feature(image_data),
+                    'image/format': bytes_feature('jpg'),
+                    'image/class/label': int64_feature(class_id),
+                    'image/height': int64_feature(height),
+                    'image/width': int64_feature(width),
+                }))
 
-                        example = tf.train.Example(features=tf.train.Features(feature={
-                            'image/encoded': bytes_feature(image_data),
-                            'image/format': bytes_feature('jpg'),
-                            'image/class/label': int64_feature(class_id),
-                            'image/height': int64_feature(height),
-                            'image/width': int64_feature(width),
-                        }))
-
-                        tfrecord_writer.write(example.SerializeToString())
+                tfrecord_writer.write(example.SerializeToString())
 
 
 def write_label_file(labels_to_class_names, dataset_dir, filename=LABELS_FILENAME):
@@ -144,25 +144,32 @@ def write_label_file(labels_to_class_names, dataset_dir, filename=LABELS_FILENAM
 
 
 def main(image_dir, example_dir):
-    mkdir_p(example_dir)
-    images, labels = match_images(image_dir)
-    class_names_to_ids = dict(zip(labels, range(len(labels))))
+    if not mkdir_p(example_dir):
+        logger.warn('Output directory already existed. Be careful for duplicate files')
 
-    x_train, x_test, y_train, y_test = train_test_split(
-        images, labels, test_size=0.1, random_state=42)
+    images, labels = match_images(image_dir)
+    class_names = sorted(set(labels))
+
+    # convert labels to ids
+    class_names_to_ids = dict(zip(class_names, range(len(class_names))))
+    class_ids = [class_names_to_ids[label] for label in labels]
+
+    x_train, x_test, y_train, y_test = train_test_split(images, class_ids, test_size=0.1, random_state=42)
 
     # First, convert the training and validation sets.
-    convert_dataset('train', x_train, class_names_to_ids,
-                    example_dir)
-    convert_dataset('validation', x_test, class_names_to_ids,
-                    example_dir)
+    convert_dataset('train', x_train, y_train, example_dir)
+    convert_dataset('validation', x_test, y_test, example_dir)
 
     # Finally, write the labels file:
-    labels_to_class_names = dict(zip(range(len(labels)), labels))
+    labels_to_class_names = dict(zip(range(len(class_names)), class_names))
     write_label_file(labels_to_class_names, example_dir)
 
 
 if __name__ == '__main__':
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    logger = logging.getLogger(__name__)
+
     parser = ArgumentParser()
 
     parser.add_argument('image_dir', help='Folder with input files')
